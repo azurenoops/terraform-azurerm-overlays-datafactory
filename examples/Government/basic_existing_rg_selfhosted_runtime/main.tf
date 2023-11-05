57d76ca87853e2ec7bc720fc54d5bdb031ea6850
John Spinella <jrspinella@users.noreply.github.com>
2023-11-05 08:19:39 -0500

    Update to examples

--- examples/Government/basic_existing_rg_selfhosted_runtime/main.tf
+++ examples/Government/basic_existing_rg_selfhosted_runtime/main.tf
@@ -12,7 +12,7 @@ module "data_factory" {
   # By default, this module will create a resource group and 
   # provide a name for an existing resource group. If you wish 
   # to use an existing resource group, change the option 
-  # to "create_key_vault_resource_group = false." The location of the group 
+  # to "create_data_factory_resource_group = false." The location of the group 
   # will remain the same if you use the current resource.
   existing_resource_group_name = azurerm_resource_group.datafactory_rg.name
   location                     = module.mod_azure_region_lookup.location_cli
